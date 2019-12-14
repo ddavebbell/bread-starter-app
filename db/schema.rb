cb1543e80d20d7bf1db53a7b846c72afd69cf675
hackerrank <me@hackerrank.com>
2019-12-13 21:51:18 -0500

    removed sqlite file again

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_03_165758) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notifications", force: :cascade do |t|
     t.integer "starter_id"
     t.string "kind"
